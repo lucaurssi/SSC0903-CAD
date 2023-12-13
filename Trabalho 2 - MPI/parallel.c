62bb9fae6dee57822496cfbfcce045577e9273ca
L Urssi <lucaurssi@gmail.com>
2023-12-13 15:31:47 -0300

    parallel not done

--- Trabalho 2 - MPI/parallel.c
+++ Trabalho 2 - MPI/parallel.c
@@ -27,55 +27,6 @@ Membros:
 
 int  main(int argc, char *argv[])  
 {
-	int npes, myrank, src, dest, msgtag, ret;
-	char *bufrecv, *bufsend;
-	MPI_Status status;
 	
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &npes); // retorna 2
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);  // retonar 0 ou 1 a depender do processo
-	
-	MPI_Get_processor_name(processor_name, &name_len);
-
-	bufrecv = (char *) malloc (strlen(MESSAGE)+1);
-   	msgtag = 1;  // dois processos executam aqui
-	if ( myrank == 0) 
-	{ // para o processo 0
-        printf("There are %d processes.\n", npes);
-		printf("I am process %d. Message with %d char. \n", myrank, (int)strlen(MESSAGE));
-		fflush(0);
-		
-		bufsend = (char *) malloc (strlen(MESSAGE)+1);
-        strcpy(bufsend,MESSAGE);
-		
-		src = dest = 1;
-		MPI_Send(bufsend, strlen(bufsend)+1, MPI_CHAR, dest, msgtag, MPI_COMM_WORLD);
-		
-		msgtag = 2;
-		MPI_Recv(bufrecv, strlen(MESSAGE)+1, MPI_CHAR, src, msgtag, MPI_COMM_WORLD, &status);
-		
-		printf("Master received message from slave: %s with %d chars. From host %s \n", bufrecv, (int)strlen(bufrecv), processor_name);
-	
-	}
-	else 
-	{   // para o processo 1
-         printf("There are %d processes. I am process %d \n", npes, myrank);
-		// fflush(0);
-		bufsend = (char *) malloc (strlen(MESSAGE)+1);
-		src = dest = 0;
-		MPI_Recv(bufrecv, strlen(MESSAGE)+1, MPI_CHAR, src, msgtag, MPI_COMM_WORLD, &status);
-		msgtag = 2;
-		strcpy(bufsend, bufrecv);
-		MPI_Send(bufsend, strlen(MESSAGE)+1, MPI_CHAR, dest, msgtag, MPI_COMM_WORLD);
-		free(bufsend);
-	} 
-	free(bufrecv);
-	fflush(0);
-	ret = MPI_Finalize();
-	if (ret == MPI_SUCCESS)
-		printf("MPI_Finalize success from myrank %d from host %s\n", myrank, processor_name);
 	return(0);
 }

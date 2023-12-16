a11d4f392947980642ded9486f539f03f64307bd
Cletor <banana1234>
2023-12-16 14:36:00 -0300

    final ajusts

--- Trabalho 2 - MPI/parallel.c
+++ Trabalho 2 - MPI/parallel.c
@@ -15,84 +15,99 @@ Membros:
 #include <stdlib.h>
 #include <mpi.h>
 #include <time.h>
+#incldue <string.h>
 
 #define PRINT 0
 
 
 int main(int argc, char *argv[]){
-	int npes, myrank, ret;
-	
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
 	
 	int MAT_SIZE = atoi(argv[1]);
 	
-
-    MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &npes); // retorna o numero de processos
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);  // retonaro rank do processo
+	MPI_Init(&argc, &argv);
 	
-	MPI_Get_processor_name(processor_name, &name_len);
-
 	// create matrix - initialized with zeros
 	int **mat = (int**)calloc (MAT_SIZE, sizeof (int*));
 	for (int i=0; i < MAT_SIZE; i++)
 		mat[i] = (int*)calloc (MAT_SIZE, sizeof (int));
 	
-	// rank 0 enche a matriz 
-	for(int i=0; i<MAT_SIZE; i++)
-		for(int j=0; j<MAT_SIZE; j++)
-			mat[i][j] = i + j;
- 
+	clock_t time = clock();
+	double total_time;
 	
+	int npes, myrank, ret;
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int name_len;
+	
+	MPI_Comm_size(MPI_COMM_WORLD, &npes); // retorna o numero de processos
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);  // retonaro rank do processo	
+	MPI_Get_processor_name(processor_name, &name_len);
+	
+	int mat_size_por_processo = MAT_SIZE  / npes;
+    int inicio = myrank * mat_size_por_processo;	
 
 	int max = 0;
 	int min = MAT_SIZE*2;
-	int sum = 0;
+	long int sum = 0;
 	int *sum_lin = (int*)calloc (MAT_SIZE, sizeof (int));
 	int *sum_col = (int*)calloc (MAT_SIZE, sizeof (int));
 	
-	clock_t time = clock();
-
-    int mat_size_por_processo = MAT_SIZE  / npes;
-    int inicio = myrank * mat_size_por_processo;
-	
-
-	for(int i=inicio; i<inicio + mat_size_por_processo; i++)
-		for(int j=0; j<MAT_SIZE; j++){
-			if(mat[i][j] > max) 
-				max = mat[i][j];
-			else 
-				if(mat[i][j] < min) 
-					min = mat[i][j];
-			
-			sum += mat[i][j];
-			
-			sum_lin[j] += mat[i][j];
-			sum_col[i] += mat[i][j];
-		}
 	int global_max=0;
 	int global_min=0;
-	int global_sum=0;
+	long int global_sum=0;
 	int *global_lin = (int*)calloc (MAT_SIZE, sizeof (int));
 	int *global_col = (int*)calloc (MAT_SIZE, sizeof (int));
 	
+	for(int k=0; k<100; k++){ // multiple runs
 	
-	MPI_Reduce(&max, &global_max, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&min, &global_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
-	MPI_Reduce(&sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Reduce(sum_lin, global_lin, MAT_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	MPI_Reduce(sum_col, global_col, MAT_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		max = 0;
+		min = MAT_SIZE*2;
+		sum = 0;
+		memset(sum_lin, 0, MAT_SIZE * sizeof(int));
+		memset(sum_col, 0, MAT_SIZE * sizeof(int));
+		
+		global_max=0;
+		global_min=0;
+		global_sum=0;
+		memset(global_lin, 0, MAT_SIZE * sizeof(int));
+		memset(global_col, 0, MAT_SIZE * sizeof(int));
+		
+		// inside loop because its improved with parallelism
+		for(int i=inicio; i<inicio + mat_size_por_processo; i++)
+			for(int j=0; j<MAT_SIZE; j++)
+				mat[i][j] = i + j;
+		
+		// actual loop
+		for(int i=inicio; i<inicio + mat_size_por_processo; i++)
+			for(int j=0; j<MAT_SIZE; j++){
+				if(mat[i][j] > max) 
+					max = mat[i][j];
+				else 
+					if(mat[i][j] < min) 
+						min = mat[i][j];
+				
+				sum += mat[i][j];
+				
+				sum_lin[j] += mat[i][j];
+				sum_col[i] += mat[i][j];
+			}
+		
+		MPI_Reduce(&max, &global_max, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+		MPI_Reduce(&min, &global_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+		MPI_Reduce(&sum, &global_sum, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(sum_lin, global_lin, MAT_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(sum_col, global_col, MAT_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	}
 	
 	
-	
-	double total_time = (double)(clock() - time)/CLOCKS_PER_SEC;
+	total_time = (double)(clock() - time)/CLOCKS_PER_SEC;
+	if(myrank == 0) 
+		printf("time: %f\n", total_time);	
 	
 	if(myrank == 0 && PRINT){
 		
 		printf("max : %d\n", global_max);
 		printf("min : %d\n", global_min);
-		printf("sum : %d\n", global_sum);
+		printf("sum : %ld\n", global_sum);
 		printf("global_lin :\n");
 		for(int i=0; i<MAT_SIZE; i++)
 			;//printf("%d ", global_lin[i]);
@@ -100,8 +115,6 @@ int main(int argc, char *argv[]){
 		for(int i=0; i<MAT_SIZE; i++)
 			;//printf("%d ", global_col[i]);
 	}
-	if(myrank == 0) 
-		printf("time: %f\n", total_time);	
 	
     ret = MPI_Finalize();
 	
--- Trabalho 2 - MPI/sequential.c
+++ Trabalho 2 - MPI/sequential.c
@@ -13,6 +13,7 @@ Membros:
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#inclue <string.h>
 
 #define PRINT 0
 
@@ -25,40 +26,52 @@ int main(int argc, char *argv[]){
 	for (int i=0; i < MAT_SIZE; i++)
 		mat[i] = (int*)calloc (MAT_SIZE, sizeof (int));
 	
-	// populating 'mat' with i + j
-	for(int i=0; i<MAT_SIZE; i++)
-		for(int j=0; j<MAT_SIZE; j++)
-			mat[i][j] = i + j;
+	clock_t time = clock();
+	double total_time;
 	
 	int max = 0;
 	int min = MAT_SIZE*2;
-	int sum = 0;
+	long int sum = 0;
 	int *sum_lin = (int*)calloc (MAT_SIZE, sizeof (int));
 	int *sum_col = (int*)calloc (MAT_SIZE, sizeof (int));
+    
+	for(int k=0; k<100; k++){ // multiple runs
+		
+		max = 0;
+		min = MAT_SIZE*2;
+		sum = 0;
+		memset(sum_lin, 0, MAT_SIZE * sizeof(int));
+		memset(sum_col, 0, MAT_SIZE * sizeof(int));
+		
+		
+		// populating 'mat' with i + j
+		for(int i=0; i<MAT_SIZE; i++)
+			for(int j=0; j<MAT_SIZE; j++)
+				mat[i][j] = i + j;
+		
+		// actual loop
+		for(int i=0; i<MAT_SIZE; i++)
+			for(int j=0; j<MAT_SIZE; j++){
+				if(mat[i][j] > max) 
+					max = mat[i][j];
+				else 
+					if(mat[i][j] < min) 
+						min = mat[i][j];
+				
+				sum += mat[i][j];
+				
+				sum_lin[j] += mat[i][j];
+				sum_col[i] += mat[i][j];
+			}
+	}
 
-    clock_t time = clock();
-
-	for(int i=0; i<MAT_SIZE; i++)
-		for(int j=0; j<MAT_SIZE; j++){
-			if(mat[i][j] > max) 
-				max = mat[i][j];
-			else 
-				if(mat[i][j] < min) 
-					min = mat[i][j];
-			
-			sum += mat[i][j];
-			
-			sum_lin[j] += mat[i][j];
-			sum_col[i] += mat[i][j];
-		}
-
-    double total_time = (double)(clock() - time)/CLOCKS_PER_SEC;
+    total_time = (double)(clock() - time)/CLOCKS_PER_SEC;
     printf("time: %f\n", total_time);	
 
     if(PRINT){
 	    printf("max : %d\n", max);
 	    printf("min : %d\n", min);
-	    printf("sum : %d\n", sum);
+	    printf("sum : %ld\n", sum);
 	    printf("sum_lin :\n");
 	    for(int i=0; i<MAT_SIZE; i++)
 		    ;//printf("%d ", sum_lin[i]);
